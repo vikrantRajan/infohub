ade5221af72c42988a0dc771437d069e08785ab0
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2020-09-11 00:16:12 -0700

    back to top button

--- src/components/footer-component/footer.js
+++ src/components/footer-component/footer.js
@@ -10,17 +10,24 @@ render(){
     return (<footer className="footerSection">
         <ScrollArrow/>
         <div className="container">
-        <div className="footer-logo">
-            <img src={footerLogo} className="footer-logo-image" alt="infohub_logo"/>
+            <div className="footer-logo">
+                <img src={footerLogo} className="footer-logo-image" alt="infohub_logo"/>
+                
+            </div>
+            <div className="footer-text">
             
-        </div>
-        <div className="footer-text">
-        
-            This is a platform created by students for students.<br></br>
-            Our goal is to share remote learning resources for people to gain <br></br>
-            hard skills in order to thrive in the digital and creative industries.
+                This is a platform created by students for students.<br></br>
+                Our goal is to share remote learning resources for people to gain <br></br>
+                hard skills in order to thrive in the digital and creative industries.
 
-        </div>
+            </div>
+            <div className="footer-text-mobile">
+
+                This is a platform created by students for students.
+                Our goal is to share remote learning resources for people to gain 
+                hard skills in order to thrive in the digital and creative industries.
+
+            </div>
         </div>
 
     </footer>)
